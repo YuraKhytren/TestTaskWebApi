d820c05b48515e8d64d1ff3fa2fbc7e291225363
Yuri_Khytren <dvigokgp@gmail.com>
2022-06-14 20:02:29 +0300

    add Controller

---
+++ TestTask/TestTask/Controllers/IncidentController.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TestTask.Models;
+using TestTask.Services.Interface;
+using TestTask.ViewModel;
+
+namespace TestTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IncidentController : ControllerBase
+    {
+        private readonly ICrudService<Incident> _service;
+        private readonly IMapper _mapper;
+        public IncidentController(ICrudService<Incident> service, IMapper mapper)
+        {
+            _mapper = mapper;
+            _service = service;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<IncidentViewModel>> GetAllAsync() 
+        {
+            return _mapper.Map<IEnumerable<IncidentViewModel>>(await _service.GetAllAsync());
+        }
+
+        [HttpGet("id")]
+        public async Task<ActionResult<IncidentViewModel>> GetByIdAsync(string id) 
+        { 
+            Incident model = await _service.GetByIdAsync(id);
+
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            return new ObjectResult(_mapper.Map<IncidentViewModel>(model));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<IncidentViewModel>> CreateAsync(IncidentViewModel model) 
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_mapper.Map<IncidentViewModel>(await _service.CreateAsync(_mapper.Map<Incident>(model))));
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<IncidentViewModel>> UpdateAsync(IncidentViewModel model)
+        {
+
+        }
+    }
+}
--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -2,12 +2,22 @@
 
 using Microsoft.EntityFrameworkCore;
 using TestTask.DataBase;
+using TestTask.Mapping;
+using TestTask.Models;
+using TestTask.Services;
+using TestTask.Services.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddTransient<ICrudService<Account>, AccountService>();
+builder.Services.AddTransient<ICrudService<Contact>, ContactService>();
+builder.Services.AddTransient<ICrudService<Incident>, IncidentService>();
+builder.Services.AddAutoMapper(typeof(Program), typeof(AccountToAccountViewModelProfile), typeof(ContactToContactViewModelProfile), typeof(IncidentToIncidentViewModelProfile));
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
